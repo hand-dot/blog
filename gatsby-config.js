3db600b6a8ecb9ef11c5aadb67c421adb2088ade
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-04-29 00:38:13 +0900

    minor fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,16 +44,16 @@ module.exports = {
           },
           `gatsby-remark-autolink-headers`,
           {
-            resolve: "gatsby-remark-embed-snippet",
+            resolve: 'gatsby-remark-embed-snippet',
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
               directory: `${__dirname}/examples/`,
             },
           },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: true,
@@ -63,17 +63,17 @@ module.exports = {
           {
             resolve: 'gatsby-remark-emojis',
             options: {
-              active : true,
-              size   : 64,
-              styles : {
-                display      : 'inline',
-                margin       : '0',
-                'margin-top' : '1px',
-                position     : 'relative',
-                top          : '5px',
-                width        : '25px'
-              }
-            }
+              active: true,
+              size: 64,
+              styles: {
+                display: 'inline',
+                margin: '0',
+                'margin-top': '1px',
+                position: 'relative',
+                top: '5px',
+                width: '25px',
+              },
+            },
           },
         ],
       },
@@ -83,8 +83,10 @@ module.exports = {
       options: {
         accessToken: process.env.ACCESS_TOKEN,
         userName: process.env.USER_NAME,
-        excludedPostIds: process.env.EXCLUDED_POST_IDS.split(',')
-      }
+        excludedPostIds: process.env.EXCLUDED_POST_IDS
+          ? process.env.EXCLUDED_POST_IDS.split(',')
+          : [],
+      },
     },
     `gatsby-remark-headings-detail`,
     `gatsby-remark-and-qiita`,
@@ -107,47 +109,47 @@ module.exports = {
         background_color: '#333',
         theme_color: '#d23d29',
         display: 'minimal-ui',
-        "icons": [
+        icons: [
           {
-            "src": "icons/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
+            src: 'icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
+            src: 'icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-128x128.png",
-            "sizes": "128x128",
-            "type": "image/png"
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
+            src: 'icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
           },
           {
-            "src": "icons/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
         ],
       },
     },
@@ -174,8 +176,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-webpack-bundle-analyzer`,
       options: {
-        openAnalyzer: false
-      }
-    }
+        openAnalyzer: false,
+      },
+    },
   ],
 }
